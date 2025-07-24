da7997b22b1d88d37be17a6dff8c578d2d3ab983
Michael An <1822852997@qq.com>
2025-07-24 15:34:10 +0800

    [2.2] add login dialog in novel reader

--- react/src/page2/Page2Component.jsx
+++ react/src/page2/Page2Component.jsx
@@ -1,7 +1,9 @@
 import App from 'genshin-novel-reader';
 
 const Page2Component = () => {
-  return (<App/>);
+  return (
+    <App mode={'online'} server={'http://127.0.0.1:8081'} />
+  );
 };
 
 export default Page2Component;
