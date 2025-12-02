b4a7ec9fe46571d28666709d47b194e1852e0928
Michael An <1822852997@qq.com>
2025-12-02 23:01:52 +0800

    [2.3] fix register use static url

--- react/src/reader/index.jsx
+++ react/src/reader/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'genshin-novel-reader';
+import CONFIG from '../../config/config.json';
+import LOCAL_CONFIG from '../../config/config-local.json';
+
+const config = LOCAL_CONFIG || CONFIG;
 
 const root = ReactDOM.createRoot(document.getElementById('react-app'));
 
 root.render(
   <App
     mode={'online'}
-    server={'http://127.0.0.1:8081'}
+    server={`http://${config.expressHost}:${config.expressPort}`}
   />
 );
